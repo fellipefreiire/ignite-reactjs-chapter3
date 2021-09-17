ad29ce77a4f61400222b90276360ef013ff1fdab
Felipe <fellipefreiire3@gmail.com>
2021-09-17 15:22:47 -0300

    adding sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import styles from "../styles/home.module.scss";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className={styles.title}>
+        Hello <span>World</span>
+      </h1>
     </div>
   );
 }
